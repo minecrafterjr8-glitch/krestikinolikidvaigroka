1d4fdb1487642cd3ffc5aa0b1c5ee8a274179c1e
minecrafterjr8-glitch <minecrafterjr8@gmail.com>
2026-05-02 11:09:16 +0100

    waiting sign

--- front/src/pages/LoginPage.jsx
+++ front/src/pages/LoginPage.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react"
 
 
 export default function LoginPage() {
+    const [isWaiting, setWaiting] = useState(false)
+    function joinHandler() {
+        setWaiting(true)
+    }
     return (
         <>
-            страница авторизации
+            <div className="loginPage"><button onClick={joinHandler} className="join-button">присоединиться</button>
+                {isWaiting && <h1 className="info-heading">Пожалуйста подождите другого игрока.</h1>}
+                
+            </div>
         </>
     )
 }
