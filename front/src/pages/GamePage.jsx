276b032f4809eb01672a44ae20384f2e46676aa1
foxkodland <foxkodland@gmail.com>
2026-05-16 13:31:11 +0300

    rename const

--- front/src/pages/GamePage.jsx
+++ front/src/pages/GamePage.jsx
@@ -6,16 +6,16 @@ import { API_URL } from "../config";
 
 export default function GamePage() {
     const params = useParams();
-    const matchIdd = params.id;
+    const matchId = params.id;
     const [info, setInfo] = useState("X делает первый пук");
-    const [machData, setMachData] = useState();
+    const [matchData, setMatchData] = useState();
 
     async function apiGetMach() {
-        const url = API_URL + "/match/" + matchIdd
+        const url = API_URL + "/match/" + matchId
         const response = await fetch(url, {headers: {authorisation: localStorage.getItem("uuid")}});
         const result = await response.json()
         console.log(result);
-        setMachData(result)
+        setMatchData(result)
     }
 
     useEffect(()=>{
